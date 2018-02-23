81ffa4b9acde891b867df46bd9b50934402c8429
himanshu <hp746123@gmail.com>
2018-02-23 06:18:16 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require ('express');
+const hbs = require ('hbs');
+const fs = require ('fs');
+
+var app = express ();
+
+hbs.registerPartials (__dirname + '/views/partials');
+app.set ('view engine', 'hbs');
+
+app.use ((req, res, next) => {
+    var now = new Date ().toString ();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile ('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to write to the file');
+        }
+    });
+    next ();
+});
+
+// app.use ((req, res, next) => {
+//     res.render ('maintenance.hbs');
+// });
+
+app.use (express.static (__dirname + '/public'));
+
+hbs.registerHelper ('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper ('screemIt', (text) => {
+    return text.toUpperCase();
+})
+
+app.get ('/', (req, res) => {
+    res.render ('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to my website',
+        currentYear: new Date ().getFullYear ()
+    })
+});
+
+app.get ('/about', (req, res) => {
+    res.render ('about.hbs', {
+        pageTitle: 'About Page',
+        currentYear: new Date ().getFullYear ()
+    });
+});
+
+app.get ('/bad', (req, res) => {
+    res.send ({
+        errorMessage: 'Unable to handle request'
+    });
+});
+
+app.listen (3000);
